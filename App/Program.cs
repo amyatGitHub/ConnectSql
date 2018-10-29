805d7506e926bdd0734c0e23221983dfc4e58ad0
amyatGitHub <wyauamy@gmail.com>
2018-10-29 17:20:56 +0800

    Add files via upload

---
+++ App/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteData();
+        }
+
+        static void WriteData()
+        {
+            Person[] inputDataList = new Person[] { };
+            string[] columns = new string[] { "Name", "Age" };
+            string dbName = "dbName";
+            string tableName = "tableName";
+
+            ConnectSql.ConnectSql sqlConn = new ConnectSql.ConnectSql();
+            sqlConn.StartConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString, dbName);
+            sqlConn.InsertDB(inputDataList, columns, tableName);
+            sqlConn.CloseConnection();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
